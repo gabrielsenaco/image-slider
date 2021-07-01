e1d9b42e0bd36a7b8c9bf814b033d4174a00f875
Gabriel Sena <gabesenacom@gmail.com>
2021-07-01 20:58:06 -0300

    Initial dist subtree commit

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { createSlider } from './slider'
-
-// TODO query images by unsplash api
-const images = ['https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=erik-mclean-ZRns2R5azu0-unsplash.jpg&w=640', 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=joey-banks-YApiWyp0lqo-unsplash.jpg&w=640']
-createSlider(images)
